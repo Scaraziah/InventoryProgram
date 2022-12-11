720a97f3823402545868e21c00dc16c462b01a51
Larry Hatcher <Scaraziah>
2022-12-11 09:00:54 -0600

    Updated post routes.

--- InventoryBackEnd/routes/post.js
+++ InventoryBackEnd/routes/post.js
@@ -4,25 +4,25 @@ const auth = require('../middleware/auth');
 const express = require('express');
 const router = express.Router();
 
-router.post('/reply/:id', async (req, res) => {
-    try {
-        const { error } = validateReply(req.body);
-        if (error) return res.status(400).send(error);
+// router.post('/reply/:id', async (req, res) => {
+//     try {
+//         const { error } = validateReply(req.body);
+//         if (error) return res.status(400).send(error);
         
-        const comment = await Post.findById(req.params.id);
-        if (!comment) return res.status(400).send(`The comment with id "${req.params.id}" does not exist.`);
+//         const comment = await Post.findById(req.params.id);
+//         if (!comment) return res.status(400).send(`The comment with id "${req.params.id}" does not exist.`);
         
-        const reply = new Reply({
-            text: req.body.text
-        })
-        comment.replies.push(reply);
+//         const reply = new Reply({
+//             text: req.body.text
+//         })
+//         comment.replies.push(reply);
         
-        await comment.save();
-        return res.send(comment);
-    } catch (ex) {
-        return res.status(500).send(`Internal Server Error: ${ex}`);
-    }
-});
+//         await comment.save();
+//         return res.send(comment);
+//     } catch (ex) {
+//         return res.status(500).send(`Internal Server Error: ${ex}`);
+//     }
+// });
 
 router.get('/', async (req,res) => {
     try{
@@ -69,11 +69,12 @@ router.post('/', async (req, res) => {
 
         const post = new Post ({
             name: req.body.name,
-            lat: req.body.lat,
-            lng: req.body.lng,
-            huntType: req.body.huntType,
-            prisePic: req.body.prisePic,
-            text: req.body.text
+            description: req.body.description,
+            itemType: req.body.itemType,
+            gold: req.body.gold,
+            silver: req.body.silver,
+            silver: req.body.copper,
+            quantaty: req.body.quantaty
         });
 
         await post.save();
@@ -122,35 +123,35 @@ router.delete('/:id', async (req, res) => {
     }
 });
 
-router.put('/likes/:id', async (req, res) => {
-    try {
-    const post = await Post.findById(req.params.id);
-        if (!post)   
-        return res.status(400).send(`The post with id "${req.params.id}" does not exist.`);
+// router.put('/likes/:id', async (req, res) => {
+//     try {
+//     const post = await Post.findById(req.params.id);
+//         if (!post)   
+//         return res.status(400).send(`The post with id "${req.params.id}" does not exist.`);
         
-        post.likes++
+//         post.likes++
         
-        await post.save();
-        return res.send(post);
-    } catch (ex) {
-        return res.status(500).send(`Internal Server Error: ${ex}`);
-    }
-});
+//         await post.save();
+//         return res.send(post);
+//     } catch (ex) {
+//         return res.status(500).send(`Internal Server Error: ${ex}`);
+//     }
+// });
 
-router.put('/dislikes/:id', async (req, res) => {
-    try {
-    const post = await Post.findById(req.params.id);
-        if (!post)   
-        return res.status(400).send(`The post with id "${req.params.id}" does not exist.`);
+// router.put('/dislikes/:id', async (req, res) => {
+//     try {
+//     const post = await Post.findById(req.params.id);
+//         if (!post)   
+//         return res.status(400).send(`The post with id "${req.params.id}" does not exist.`);
         
-        post.dislikes++
+//         post.dislikes++
         
-        await post.save();
-        return res.send(post);
-    } catch (ex) {
-        return res.status(500).send(`Internal Server Error: ${ex}`);
-    }
-});
+//         await post.save();
+//         return res.send(post);
+//     } catch (ex) {
+//         return res.status(500).send(`Internal Server Error: ${ex}`);
+//     }
+// });
 
 router.put('/prisePic/:id', async (req, res) => {
     try {
