9de3f677db7f336c6dd74a6887f7a6abb2761e1b
Larry Hatcher <Scaraziah>
2022-12-11 08:31:44 -0600

    Updated the post to make it for items and validations.

--- InventoryBackEnd/models/user.js
+++ InventoryBackEnd/models/user.js
@@ -3,22 +3,12 @@ const Joi = require('joi');
 const config = require('config');
 const jwt = require('jsonwebtoken');
 
-const replySchema = new mongoose.Schema({
-    text: {type: String, required: true, minlength: 5, maxlength: 1000},
-    timeStamp: {type: Date, default: Date.now()},
-});
-
 const postSchema = new mongoose.Schema({
     name: {type: String, required: true, minlength: 5, maxlength: 50 },
-    text: {type: String, required: true, minlength: 1, maxlength: 1000},
-    // lat: {type : Number, required: true},
-    // lng: {type : Number, required: true},
-    // huntType: {type: String, required: true, minlength: 4, maxlength: 8 },
-    // prisePic: {type: Array, required: true },
-    likes: {type: Number, default: 0},
-    dislikes: {type: Number, default: 0},
-    timeStamp: {type: Date, default: Date.now()},
-    // replies: [{type: replySchema}],
+    description: {type: String, required: true, minlength: 5, maxlength: 50 },
+    itemType: {type: String, required: true, minlength: 5, maxlength: 50 },
+    quantaty: {type: Number, required: true, minlength: 1, maxlength: 99999},
+    value: {type: Number, required: true, minlength: 1, maxlength: 99999},
 });
 
 const bioSchema = new mongoose.Schema({
@@ -46,7 +36,6 @@ userSchema.methods.generateAuthToken = function () {
 const User = mongoose.model('User',  userSchema);
 const Post = mongoose.model('Post', postSchema);
 const Bio = mongoose.model('Bio', bioSchema);
-const Reply = mongoose.model('Reply', replySchema);
 
 function validateUser(user) {
     const schema =Joi.object({
@@ -60,12 +49,10 @@ function validateUser(user) {
 function validatePost(post) {
     const schema = Joi.object({
         name: Joi.string().min(5).max(50).required(),
-        //rename lat =  and lng = lng in other areas
-        // lat: Joi.number().required(),
-        // lng: Joi.number().required(),
-        // huntType: Joi.string().min(1).max(8).required(),
-        // prisePic: Joi.array().items(Joi.string()),
-        text: Joi.string().min(1).max(1000).required(),
+        description: Joi.string().min(5).max(50).required(),
+        itemType: Joi.string().min(1).max(8).required(),
+        value: Joi.number().min(1).max(99999).required(),
+        quantaty: Joi.number().min(1).max(99999).required(),
     });
     return schema.validate(post);
 }
@@ -79,12 +66,6 @@ function validateBio(bio) {
     return schema.validate(bio);
 }
 
-function validateReply(reply) {
-    const schema = Joi.object({
-        text: Joi.string().min(2).max(1000).required()
-    });
-    return schema.validate(reply);
-}
 
 exports.User = User;
 exports.Post = Post;
